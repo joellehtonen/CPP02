c42e162e158e326cf6bdf63c973b805590d7c50d
Joel Lehtonen <jlehtone@c1r1p4.hive.fi>
2025-01-08 14:05:02 +0200

    added edge detection to bsp

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -21,6 +21,12 @@ bool bsp(Point const a, Point const b, Point const c, Point const p)
 	float	pcaArea = calculateArea(a.getX().toFloat(), a.getY().toFloat(), p.getX().toFloat(), p.getY().toFloat(), c.getX().toFloat(), c.getY().toFloat());
 	float	totalSum = pabArea + pbcArea + pcaArea;
 
+	if (pabArea == 0 || pbcArea == 0 || pcaArea == 0)
+	{
+		std::cout << "The point is on an edge" << std::endl;
+		return false;
+	}
+
 	std::cout << "Total Area is " << abcArea << std::endl;
 	std::cout << "Total Sum is " << totalSum << std::endl;
 
